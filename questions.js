1c82f8cdc67cca171ddcf5f27387f5860079a91a
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-09-15 21:42:01 -0700

    adding html, css, and js

---
+++ questions.js
@@ -0,0 +1,35 @@
+// Array for questions 
+var questions = [
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+];
+
+// Variables
+var score = 0;
+var questionsIndex = 0;
+
+// variables in working html code
+var timer = document.querySelector('#timer');
